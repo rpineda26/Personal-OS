85dbfdea1af998ba3cf61f1b705525b09a69ed1b
rpineda26 <91407317+rpineda26@users.noreply.github.com>
2023-05-01 14:01:08 +0800

    fix: uncalled start_ctors and end_ctors

--- kernel.cpp
+++ kernel.cpp
@@ -5,6 +5,15 @@ void printf(char* str)
 		VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
 
 }
+
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors()
+{
+	for(constructor* i = &start_ctors; i != end_ctors; i+=1)
+		(*i)();
+}
 extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
 {
 	printf("Personal Operating System by: Ralph Pineda");
