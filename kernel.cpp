cf06153f3b29f94e5094d57b2674a59d1a593b5a
rpineda26 <91407317+rpineda26@users.noreply.github.com>
2023-05-01 17:17:51 +0800

    add: type definitions to assign bits easier

--- kernel.cpp
+++ kernel.cpp
@@ -1,6 +1,7 @@
+#include "types.h"
 void printf(char* str)
 {
-	unsigned short* VideoMemory = (unsigned short*)0xb8000;
+	static uint16_t* VideoMemory = (uint16_t*)0xb8000;
 	for(int i=0; str[i] != '\0'; i++)
 		VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
 
@@ -14,7 +15,7 @@ extern "C" void callConstructors()
 	for(constructor* i = &start_ctors; i != &end_ctors; i+=1)
 		(*i)();
 }
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
 {
 	printf("Personal Operating System by: Ralph Pineda");
 
